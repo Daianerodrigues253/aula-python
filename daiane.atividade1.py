098d958c7e12fd07db828ee86c1dc69f496ebb66
Daiane Rodrigues <daianerodriguesribeiro22@gmail.com>
2025-05-12 21:36:20 -0300

    Add files via upload

---
+++ daiane.atividade1.py
@@ -0,0 +1,89 @@
+alunos = []
+
+for i in range(1):
+    nome=input("digite o nome do aluno: ")
+    nota1 = float(input("digite a primeira nota: "))
+    nota2 = float(input("digite a segunda nota: "))
+    media = (nota1 + nota2) /2
+alunos.append({
+    "nome": nome,
+    "nota1": nota1,
+    "nota2": nota2,
+    "media": media
+})
+print("\n---resultado final---")
+
+for aluno in alunos : 
+    print(f"{aluno['nome']} - nota1:{aluno['nota1']} | nota2:{aluno['nota2']} | media: {aluno['media']:.2f}")
+
+alunos = []
+
+for i in range(1):
+    nome=input("digite o nome do aluno: ")
+    nota1 = float(input("digite a primeira nota: "))
+    nota2 = float(input("digite a segunda nota: "))
+    media = (nota1 + nota2) /2
+alunos.append({
+    "nome": nome,
+    "nota1": nota1,
+    "nota2": nota2,
+    "media": media
+})
+print("\n---resultado final---")
+
+for aluno in alunos : 
+    print(f"{aluno['nome']} - nota1:{aluno['nota1']} | nota2:{aluno['nota2']} | media: {aluno['media']:.2f}")
+
+alunos = []
+
+for i in range(1):
+    nome=input("digite o nome do aluno: ")
+    nota1 = float(input("digite a primeira nota: "))
+    nota2 = float(input("digite a segunda nota: "))
+    media = (nota1 + nota2) /2
+alunos.append({
+    "nome": nome,
+    "nota1": nota1,
+    "nota2": nota2,
+    "media": media
+})
+print("\n---resultado final---")
+
+for aluno in alunos : 
+    print(f"{aluno['nome']} - nota1:{aluno['nota1']} | nota2:{aluno['nota2']} | media: {aluno['media']:.2f}")
+
+alunos = []
+
+for i in range(1):
+    nome=input("digite o nome do aluno: ")
+    nota1 = float(input("digite a primeira nota: "))
+    nota2 = float(input("digite a segunda nota: "))
+    media = (nota1 + nota2) /2
+alunos.append({
+    "nome": nome,
+    "nota1": nota1,
+    "nota2": nota2,
+    "media": media
+})
+print("\n---resultado final---")
+
+for aluno in alunos : 
+    print(f"{aluno['nome']} - nota1:{aluno['nota1']} | nota2:{aluno['nota2']} | media: {aluno['media']:.2f}")
+
+alunos = []
+
+for i in range(1):
+    nome=input("digite o nome do aluno: ")
+    nota1 = float(input("digite a primeira nota: "))
+    nota2 = float(input("digite a segunda nota: "))
+    media = (nota1 + nota2) /2
+alunos.append({
+    "nome": nome,
+    "nota1": nota1,
+    "nota2": nota2,
+    "media": media
+})
+print("\n---resultado final---")
+
+for aluno in alunos : 
+    print(f"{aluno['nome']} - nota1:{aluno['nota1']} | nota2:{aluno['nota2']} | media: {aluno['media']:.2f}")
