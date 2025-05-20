4cae4cbcbcd848fb0f7fd3f9f83dc5d192e19de2
Daiane Rodrigues <daianerodriguesribeiro22@gmail.com>
2025-05-20 20:36:29 -0300

    Add files via upload

---
+++ trabalho.py
@@ -0,0 +1,14 @@
+cliente = []
+
+for i in range (3):
+    pessoa=input("digite o nome da pessoa: ")
+    filme= input("digite o nome do filme : ")
+    nota= float(input("digite a nota (0 a 10): "))
+
+    if  0<= nota <=10:
+         print("nota valida!")
+    else:
+         print("nota invalida")
+      
+
+   
