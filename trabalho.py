cb7a65362e2ccfd88bac640ad2d86407a5de76ca
Daiane Rodrigues <daianerodriguesribeiro22@gmail.com>
2025-05-20 21:04:47 -0300

    Add files via upload

--- trabalho.py
+++ trabalho.py
@@ -1,14 +1,28 @@
-cliente = []
+cadastro = []
 
-for i in range (3):
-    pessoa=input("digite o nome da pessoa: ")
-    filme= input("digite o nome do filme : ")
-    nota= float(input("digite a nota (0 a 10): "))
+for i in range(3):
+    nome = input('Digite seu nome: ')
+    filme = input('Digite o nome do filme: ')
 
-    if  0<= nota <=10:
-         print("nota valida!")
-    else:
-         print("nota invalida")
-      
+    while True:
+        try:
+            nota = float(input('Digite a nota do filme (0 a 10): '))
+            if 0 <= nota <= 10:
+                break  # vai fechar o looping
+            else:
+                print('Nota inválida! Tente novamente.')
+        except ValueError:
+            print('Digite um número válido!')
 
-   
+
+    cadastro.append({
+        'nome': nome,
+        'filme': filme,
+        'nota': nota
+    })
+    
+
+print('\nCadastro finalizado:')
+for item in cadastro:
+    print(f"Nome: {item['nome']}, Filme: {item['filme']}, Nota: {item['nota']}")
+
